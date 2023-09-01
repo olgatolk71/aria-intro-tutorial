228ab47d2976e646803f5017687773382caf110c
Andrew Nordlund <github@nordburg.ca>
2023-09-01 18:01:00 -0400

    WIP:Working on assignment 3

--- assignment3/assignment3.js
+++ assignment3/assignment3.js
@@ -7,6 +7,5 @@ if (submitBtn) {
 }
 
 function sendComments (e) {
-	let msg = "Thank you for sending your comments. Your comments have already been thrown into the dustbin of the Interwebs, and will be ignored at once!"	;
-
+	
 } // End of sendComments
