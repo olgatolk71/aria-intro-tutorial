b6c324bd7c430b576e5073418c8125d66f734acb
Andrew Nordlund <github@nordburg.ca>
2023-09-01 11:03:45 -0400

    Finished Assignment 2

---
+++ assignment2/assignment2.js
@@ -0,0 +1,11 @@
+var submitBtn = null;
+
+submitBtn = document.getElementById("submitBtn");
+
+if (submitBtn) {
+	submitBtn.addEventListener("click", sendComments, false);
+}
+
+function sendComments (e) {
+	
+} // End of sendComments
