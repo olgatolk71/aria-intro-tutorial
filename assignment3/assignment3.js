3b752c9ac6263733ae67587e839d25ac29349303
Andrew Nordlund <github@nordburg.ca>
2023-09-02 14:17:01 -0400

    I think I finished the homework.

--- assignment3/assignment3.js
+++ assignment3/assignment3.js
@@ -1,11 +1,38 @@
-var submitBtn = null;
+let tabBtns = [];
+let tabPanels = {};
 
-submitBtn = document.getElementById("submitBtn");
-
-if (submitBtn) {
-	submitBtn.addEventListener("click", sendComments, false);
+tabBtns = Array.from(document.querySelectorAll(".tabBtn"));
+for (let i =0 ; i < tabBtns.length; i++) {
+	tabBtns[i].addEventListener("click", exposeTab, false);
+	tabBtns[i].addEventListener("keyup", selectTab, false);
 }
 
-function sendComments (e) {
-	
-} // End of sendComments
+let x = document.querySelectorAll(".tabPanel");
+for (let i =0 ; i < x.length; i++) {
+	tabPanels[x[i].id] = x[i];
+}
+
+function selectTab (e) {
+	if (e.keyCode == 39) {
+		tabBtns[(tabBtns.indexOf(e.target) + 1) % 3].focus();
+	} else if (e.keyCode == 37) {
+		tabBtns[((tabBtns.indexOf(e.target) - 1) < 0 ? 2 : tabBtns.indexOf(e.target) - 1)].focus();
+	}
+} // End of selecTab
+
+function exposeTab(e) {
+	let tabPanelID = e.target.id.replace("Btn", "Panel");
+
+	for (var i = 0; i < tabBtns.length; i++) {
+		if (tabBtns[i].id == e.target.id) {
+			tabPanels[tabPanelID].classList.remove("hidden");
+			tabBtns[i].removeAttribute("tabindex");
+			tabBtns[i].parentNode.classList.add("selectedTab");
+		} else {
+			tabPanels[tabBtns[i].id.replace("Btn", "Panel")].classList.add("hidden");
+			tabBtns[i].setAttribute("tabindex", "-1");
+			tabBtns[i].parentNode.classList.remove("selectedTab");
+		}
+	}
+} // End of exposeTab
+
