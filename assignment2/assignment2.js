ed0131da3b7e8504886b1dc4950acee0d300b0dd
olgatolk71 <64431297+olgatolk71@users.noreply.github.com>
2023-11-08 18:44:18 -0500

    Update assignment2.js

--- assignment2/assignment2.js
+++ assignment2/assignment2.js
@@ -14,12 +14,19 @@ if (submitBtn) {
         console.error ("Did not get the submit button for some reason.");
 }
 
+document.getElementById('submitBtn').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the form from submitting
+    // Add code to handle form submission, e.g., display the status message
+    // Keep focus on the Submit button
+    document.getElementById('submitBtn').focus();
+ });
+
 function sendComments (e) {
-	//e.preventDefault();  // When uncommented, this will prevent the form from submitting when the button is clicked
+	e.preventDefault();  // When uncommented, this will prevent the form from submitting when the button is clicked
 
 	// This is the message we want added to the aria live region
 	let msg = "Thank you for sending your comments. Your comments have already been thrown into the dustbin of the Interwebs, and will be ignored at once!"	;
 
-	//messageCentre.innerText = msg;  // Uncomment to add the msg text to the live region.
+	messageCentre.innerText = msg;  // Uncomment to add the msg text to the live region.
 	
 } // End of sendComments
